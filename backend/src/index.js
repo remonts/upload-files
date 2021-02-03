6ee84e885cd8550b4bbeb7683dda36243526ba13
remonts <monteiro-renan@hotmail.com>
2021-02-03 19:28:10 -0300

    Solucionado problema de conexão com o mongoose.

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,12 +4,12 @@ const morgan = require('morgan');
 //const routes = require('./routes');
 const mongoose = require('mongoose');
 
-
 const app = express(); 
 
 // DATABASE SETUP
-mongoose.connect('mongodb://localhost:27017/upload', {
+mongoose.connect('mongodb://127.0.0.1:27017/upload', {
     useNewUrlParser: true,
+    useUnifiedTopology: true,
 });
 
 app.use(express.json());
