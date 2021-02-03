60db77d765199de5b448a7d6b84ad8283d2f4cdf
remonts <monteiro-renan@hotmail.com>
2021-02-02 21:12:01 -0300

    Concluída configuração inicial do multer.js

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,6 +1,10 @@
 const routes = require('express').Router();
+const multer = require('multer');
+const multerConfig = require('./config/multer');
 
-routes.get('/', (req, res) => {
+routes.post("/posts", multer(multerConfig).single('file'), (req, res) => {
+    console.log(req.file);
+    
     return res.json({ hello: 'Renan!'});
 });
 
