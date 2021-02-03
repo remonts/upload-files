e5a513a0352410767c949b0814d28beb4c72e126
remonts <monteiro-renan@hotmail.com>
2021-02-03 06:58:05 -0300

    Configurado conexão com mongodb.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,16 @@
 const { json } = require('express');
 const express = require('express');
 const morgan = require('morgan');
-const routes = require('./routes');
+//const routes = require('./routes');
+const mongoose = require('mongoose');
 
-const app = express();
+
+const app = express(); 
+
+// DATABASE SETUP
+mongoose.connect('mongodb://localhost:27017/upload', {
+    useNewUrlParser: true,
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,10 +2,19 @@ const routes = require('express').Router();
 const multer = require('multer');
 const multerConfig = require('./config/multer');
 
-routes.post("/posts", multer(multerConfig).single('file'), (req, res) => {
-    console.log(req.file);
+const Post = require('./models/Post');
+
+routes.post("/posts", multer(multerConfig).single('file'), async (req, res) => {
+    const { originalname: name, size, filename: key } = req.file;
+
+    const post = await Post.create({
+        name,
+        size,
+        key,
+        url: '',
+    });
     
-    return res.json({ hello: 'Renan!'});
+    return res.json(post);
 });
 
 module.exports = routes;
